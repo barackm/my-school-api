5aa6d55dbd2d3f39d30278d06343ef95d15a7427
Baraka mukelenga <baraka.m@code-care.pro>
2024-09-07 15:53:09 +0200

    Refactor training types route and service modules

--- app/modules/training_types/route.py
+++ app/modules/training_types/route.py
@@ -2,7 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from .schema import TrainingTypeResponse, TrainingTypeCreate
 from typing import List
 from sqlalchemy.orm import Session
-from .service import get_training_types, create_training_type, get_training_type_by_name
+from .service import (
+    get_training_types,
+    create_training_type,
+    get_training_type_by_name,
+    find_training_type_by_id,
+    update_training_type,
+    delete_training_type,
+)
 from app.db.database import get_db
 
 router = APIRouter()
@@ -17,5 +24,37 @@ def all(db: Session = Depends(get_db)):
 def create(training: TrainingTypeCreate, db: Session = Depends(get_db)):
     existing_training = get_training_type_by_name(db, training.name)
     if existing_training:
-        raise HTTPException(status_code=400, detail="Training type already exists")
+        raise HTTPException(status_code=400, detail="Training name already exists")
     return create_training_type(db, training)
+
+
+@router.put("/{training_type_id}", response_model=TrainingTypeResponse)
+def update(
+    training_type_id: str, training: TrainingTypeCreate, db: Session = Depends(get_db)
+):
+    training_type = find_training_type_by_id(db, training_type_id)
+    if training_type is None:
+        raise HTTPException(status_code=404, detail="Training type not found")
+
+    existing_training = get_training_type_by_name(db, training.name)
+
+    if training.name is not None and existing_training:
+        raise HTTPException(status_code=400, detail="Training name already exists")
+
+    return update_training_type(db, training_type_id, training)
+
+
+@router.get("/{training_type_id}", response_model=TrainingTypeResponse)
+def by_id(training_type_id: str, db: Session = Depends(get_db)):
+    training_type = find_training_type_by_id(db, training_type_id)
+    if training_type is None:
+        raise HTTPException(status_code=404, detail="Training type not found")
+    return training_type
+
+
+@router.delete("/{training_type_id}", response_model=TrainingTypeResponse)
+def delete(training_type_id: str, db: Session = Depends(get_db)):
+    training_type = find_training_type_by_id(db, training_type_id)
+    if training_type is None:
+        raise HTTPException(status_code=404, detail="Training type not found")
+    return delete_training_type(db, training_type_id)
--- app/modules/training_types/service.py
+++ app/modules/training_types/service.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import joinedload, selectinload
+from sqlalchemy.exc import DataError
 from .schema import TrainingTypeCreate
 from .model import TrainingType
 
@@ -15,13 +16,18 @@ def get_training_types(db: Session):
 
 
 def find_training_type_by_id(db: Session, training_type_id: str):
-    training_type = (
-        db.query(TrainingType)
-        .filter(TrainingType.id == training_type_id)
-        .options(joinedload(TrainingType.levels), joinedload(TrainingType.promotions))
-        .first()
-    )
-    return training_type
+    try:
+        training_type = (
+            db.query(TrainingType)
+            .filter(TrainingType.id == training_type_id)
+            .options(
+                joinedload(TrainingType.levels), joinedload(TrainingType.promotions)
+            )
+            .first()
+        )
+        return training_type
+    except DataError:
+        return None
 
 
 def create_training_type(db: Session, training: TrainingTypeCreate):
@@ -33,4 +39,29 @@ def create_training_type(db: Session, training: TrainingTypeCreate):
 
 
 def get_training_type_by_name(db: Session, name: str):
-    return db.query(TrainingType).filter(TrainingType.name.ilike(name)).first()
+    training = db.query(TrainingType).filter(TrainingType.name.ilike(name)).first()
+    if training is None:
+        return None
+    training_type = find_training_type_by_id(db, training.id)
+    return training_type
+
+
+def update_training_type(
+    db: Session, training_type_id: str, training: TrainingTypeCreate
+):
+    training_type = find_training_type_by_id(db, training_type_id)
+    if training_type is None:
+        return None
+    training_type.name = training.name
+    db.commit()
+    db.refresh(training_type)
+    return training_type
+
+
+def delete_training_type(db: Session, training_type_id: str):
+    training_type = find_training_type_by_id(db, training_type_id)
+    if training_type is None:
+        return None
+    db.delete(training_type)
+    db.commit()
+    return training_type
